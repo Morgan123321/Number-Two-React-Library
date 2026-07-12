961aff99a83e30e6cb5575a418bffbf0072de6a3
Morgan123321 <morgan.feeney1022@gmail.com>
2026-07-12 09:26:47 -0400

    highlights

--- Library - React/src/App.js
+++ Library - React/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <Nav />
         <Landing />
+        <Highlights />
       </div>
     </Router>
   );
--- Library - React/src/components/ui/Highlight.jsx
+++ Library - React/src/components/ui/Highlight.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-const Highlight = ({icon}) => {
+const Highlight = ({icon, title, para}) => {
 return (
 <div className="highlight">
         <div className="highlight__img">
         {icon}
          </div>
-         <h3 className="highlight__subtitle">Affordable</h3>
+         <h3 className="highlight__subtitle">{title}</h3>
         <p className="highlight__para">
-         Get access to the books you purchase instantly!
+        {para}
       </p>
         </div>
 );

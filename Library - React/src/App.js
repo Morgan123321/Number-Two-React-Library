f48b74b06f29a9461fc07583096d5a89bdcba329
Morgan123321 <morgan.feeney1022@gmail.com>
2026-07-19 10:10:40 -0400

    route

--- Library - React/src/App.js
+++ Library - React/src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
-import Discounted from './components/ui/Discounted';
-import Explore from './components/Explore';
 import Footer from './components/Footer';
-import Featured from './components/Featured';
 import Highlights from './components/Highlights';
-import Landing from "./components/Landing";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import "./App.css";
@@ -15,11 +11,10 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route/>
         <Nav />
         <Route path="/" exact component={Home}/>
-        <Route path="/books" component={Books}/>
-      <Home/>
+        <Route path="/books" render={() => <Books books={books}/>}
+      
         <Footer/>
       </div>
     </Router>

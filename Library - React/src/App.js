6c5c3de76b2c89031f129fccde53f33332619977
Morgan123321 <morgan.feeney1022@gmail.com>
2026-07-19 08:18:59 -0400

    fix

--- Library - React/src/App.js
+++ Library - React/src/App.js
@@ -5,10 +5,10 @@ import Footer from './components/Footer';
 import Featured from './components/Featured';
 import Highlights from './components/Highlights';
 import Landing from "./components/Landing";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 
 function App() {

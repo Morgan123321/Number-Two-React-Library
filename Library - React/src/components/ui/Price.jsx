77237ab37c8d883e7fc685d60ca3ce1cbb7c25ff
Morgan123321 <morgan.feeney1022@gmail.com>
2026-07-23 17:58:53 -0400

    price

--- Library - React/src/components/ui/Price.jsx
+++ Library - React/src/components/ui/Price.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-const Price = ({salePrice,originalPrice}) => {
-return (
-     <div className="book__price">
-                               {book.salePrice ? (
-                                <>
-                                <span className="book__price--normal">${book.originalPrice.toFixed(2)}</span>
-                                {book.salePrice.toFixed(2)}
-                                </>
-                               ):(
-                                <>${book.originalPrice.toFixed(2)}</>
-                               )}
-                            </div>
-);
+const Price = ({ salePrice, originalPrice }) => {
+  return (
+    <div className="book__price">
+      {salePrice ? (
+        <>
+          <span className="book__price--normal">
+            ${originalPrice.toFixed(2)}
+          </span>
+          ${salePrice.toFixed(2)}
+        </>
+      ) : (
+        <>${originalPrice.toFixed(2)}</>
+      )}
+    </div>
+  );
 };
-export default Price;

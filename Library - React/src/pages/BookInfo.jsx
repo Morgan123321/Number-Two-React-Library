11be8a676e11d34f3bf8cf3a74a9b1abe98cd5fe
Morgan123321 <morgan.feeney1022@gmail.com>
2026-07-26 07:52:49 -0400

    change

--- Library - React/src/pages/BookInfo.jsx
+++ Library - React/src/pages/BookInfo.jsx
@@ -65,11 +65,15 @@ const BookInfo = ({ books }) => {
                                     Recommended Books
                                 </h2>
                             </div>
+                            <div className="books">
                             {
-                            books.filter(book=>book.rating === 5 && book.id !==id)
-                            .map(book=> <Book book={book} key={book.id}/>)
-                            }
+                            books.filter(book=>book.rating === 5 && +book.id !== +id)
+                            .slice(0,4)
+                            .map(book=> <Book book={book} key={book.id}/>
+
+                            )}                            }
                         </div>
+                    </div>
                     </div>
                 </div>
             </main>
